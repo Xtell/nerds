9fb81d8dbcb2dcca5988accd823cbb1b6a728a24
xtell <evstafyevan96@gmail.com>
2019-10-08 19:39:47 +0500

    Added style for skills section

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,13 @@
 var sliderButtons = document.querySelectorAll(".slider-button");
 var sliderItems = document.querySelectorAll(".slider-item");
-var currentSliderButton = document.querySelector(".slider-button.current-slide");
-var currentSliderItem = document.querySelector(".slider-item.current-slide");
 var addSliderButtonClickHandler = function(sliderButtons, sliderItems) {
     sliderButtons.addEventListener('click', function(evt) {
         evt.preventDefault();
-        var currentSliderButton = document.querySelector(".slider-button.current-slide");
+        var currentSliderButton = document.querySelector(".slider-button.current-slide-button");
         var currentSliderItem = document.querySelector(".slider-item.current-slide");
-        currentSliderButton.classList.remove("current-slide");
+        currentSliderButton.classList.remove("current-slide-button");
         currentSliderItem.classList.remove("current-slide");
-        sliderButtons.classList.add("current-slide");
+        sliderButtons.classList.add("current-slide-button");
         sliderItems.classList.add("current-slide");
     });
 }
