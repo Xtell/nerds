dc5790b21a0b484442716ade503acb3c45644de0
xtell <evstafyevan96@gmail.com>
2019-10-09 20:08:28 +0500

    Added modal popup

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+// Слайдер
 var sliderButtons = document.querySelectorAll(".slider-button");
 var sliderItems = document.querySelectorAll(".slider-item");
 var addSliderButtonClickHandler = function(sliderButtons, sliderItems) {
@@ -15,3 +16,17 @@ var addSliderButtonClickHandler = function(sliderButtons, sliderItems) {
 for(var i=0; i < sliderItems.length; i++) {
     addSliderButtonClickHandler(sliderButtons[i], sliderItems[i]);
 }
+// Модальное окно
+var openModalButton = document.querySelector("#modal-appeal-link");
+var modalAppeal = document.querySelector(".modal-appeal");
+var closeModalButton = modalAppeal.querySelector(".modal-close");
+var submitForm = document.querySelector("#submit-button");
+
+openModalButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modalAppeal.classList.add('modal-show');
+});
+closeModalButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modalAppeal.classList.remove('modal-show')
+});
